75a4f2b7b45351c5c05a76d806b592c9f98dba52
Wojciech Prokurat <wojtekprokurat@gmail.com>
2022-11-22 20:01:33 +0100

    EF DbContext & DbSet

---
+++ BlazorEcommerce/Server/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorEcommerce.Server.Data
+{
+    public class DataContext : DbContext 
+    {
+        public DbSet<Product> Products { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+    }
+}
