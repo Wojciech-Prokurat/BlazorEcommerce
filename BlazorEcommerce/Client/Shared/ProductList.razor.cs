6160c0e60db9390862cf6034bdf5e56d6ec1c861
Wojciech Prokurat <wojtekprokurat@gmail.com>
2022-11-27 15:44:02 +0100

    Product Service in Client

--- BlazorEcommerce/Client/Shared/ProductList.razor.cs
+++ BlazorEcommerce/Client/Shared/ProductList.razor.cs
@@ -1,15 +1,10 @@
-﻿using BlazorEcommerce.Shared;
-using System.Net.Http.Json;
-
-namespace BlazorEcommerce.Client.Shared
+﻿namespace BlazorEcommerce.Client.Shared
 {
     public partial class ProductList
     {
-        private static List<Product> products = new List<Product>();            
         protected override async Task OnInitializedAsync()
         {
-            var result = await Http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/Product");
-            products = result != null && result.Data != null ? result.Data : new List<Product>();
+            await productService.GetProducts();
         }
     }
 }
